234885242c3e4a62b7dcadc600872228aebbc337
Ross Cathcart <rdcscotland@outlook.com>
2023-02-24 08:14:17 +0000

    added allocation strategy class and friend declaration for db class

--- ee273project/inc/Database.h
+++ ee273project/inc/Database.h
@@ -2,12 +2,19 @@
 #include "Project.h"
 #include "User.h"
 
-class Database { //Friend Admin???
+class Database {
+
+public:
+	void showProjects() {};
+	void listStudents() {};
+	void listStaff() {};
 
 private:
 	std::vector<Student> studentDB;
 	std::vector<Supervisor> supervisorDB;
 	std::vector<Project> projectDB;
 
+	//provide the Admin with direct access to all attributes of Student and Supervisor
+	friend class Admin;
 
 };
