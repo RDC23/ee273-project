9b2ca01b1c42b501c07551ad6e7236d9a9c58a47
Ross Cathcart (Student) <ross.cathcart.2021@uni.strath.ac.uk>
2023-03-03 10:37:10 +0000

    added better error handling to the database cpp file)

--- project_code/Database.cpp
+++ project_code/Database.cpp
@@ -14,19 +14,32 @@ Database* Database::getInstance(){
 void loadDBfromCSV();
 
 Student* Database::getStudent(std::string name) {
-	std::vector<Student>* ptr = &this->studentDB;
-	return &*std::find_if(ptr->begin(), ptr->end(), [&](Student student) {
+	auto& student_db = this->studentDB;
+	auto stud_loc = std::find_if(student_db.begin(), student_db.end(), [&](Student& student) {
 		return student.getName() == name; });
-	
+	if (stud_loc != student_db.end()) {
+		return &*stud_loc;
+	}
+	return nullptr;	
 }
 Supervisor* Database::getSupervisor(std::string name) {
-	std::vector<Supervisor>* ptr = &this->supervisorDB;
-	return &*std::find_if(ptr->begin(), ptr->end(), [&](Supervisor supervisor) {
+	auto& sup_db = this->supervisorDB;
+	auto sup_loc = std::find_if(sup_db.begin(), sup_db.end(), [&](Supervisor& supervisor) {
 		return supervisor.getName() == name; });
-
+	if (sup_loc != sup_db.end()) {
+		return &*sup_loc; //deref iterator and return adderess of project in project DB vector
+	}
+	return nullptr;
 }
+
 Project* Database::getProject(std::string name) {
-	std::vector<Project>* ptr = &this->projectDB;
-	return &*std::find_if(ptr->begin(), ptr->end(), [&](Project project) {
+	//returns a pointer to a project in the database
+	auto& proj_db = this->projectDB;
+	auto project_loc = std::find_if(proj_db.begin(), proj_db.end(), [&](Project& project) {
 		return project.getTitle() == name; });
+	if (project_loc != proj_db.end()) {
+		//project has been found, return its address
+		return &*project_loc;
+	}
+	return nullptr; //we have not found the project
 }
